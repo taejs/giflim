0895893fa3c1260d9d1abe058274ea71b987737a
fed-gren <deveb22@gmail.com>
2019-05-18 13:38:29 +0900

    init: working component

---
+++ src/components/SearchGif.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+
+class SearchGif extends Component {
+
+}
+export default SearchGif;
--- src/components/WorkingPage.js
+++ src/components/WorkingPage.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
+import SearchGif from "./SearchGif";
+import Timeline from "./Timeline";
+
 class WorkingPage extends Component {
-    
+  render() {
+    return (
+      <div>
+        <SearchGif />
+        <Timeline />
+      </div>
+    );
+  }
 }
 export default WorkingPage;
