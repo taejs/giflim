5bf4df95a98f00b829b6634f5a4cac8eb468fae7
Tae <smireat1@gmail.com>
2019-05-18 15:15:55 +0900

    feat(timeline) : show selected gif

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import Store from '../lib/Store'
 
 class Timeline extends Component {
+  constructor() {
+    super();
+    console.log(Store);
+  }
   render() {
+    var seletedGif = Store.getState('selectedGif');
+    debugger;
     return (
       <div>
-        
+        {seletedGif.map((item) => <img src={item.url}></img>)}
       </div>
     )
   }
