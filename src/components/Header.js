ed3bc4ddd15f299531fd0e1ab43f5ff5249e7531
fed-gren <deveb22@gmail.com>
2019-05-18 15:15:50 +0900

    feat: create a Header
    - Title만 있는 헤더 작성

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "../styles/Header.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>GIFlim</h1>
+      </div>
+    )
+  }
+}
