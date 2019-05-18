22eeb171f69f8b8fed530da861d06097b63df7eb
fed-gren <deveb22@gmail.com>
2019-05-18 16:59:40 +0900

    feat: submit, back button
    - 결과 화면, 목록 화면 가기 위한 버튼 추가

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import Store from '../lib/Store'
+import Store from "../lib/Store";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 class Timeline extends Component {
   constructor() {
@@ -7,13 +9,20 @@ class Timeline extends Component {
     console.log(Store);
   }
   render() {
-    var seletedGif = Store.getState('selectedGif');
+    var seletedGif = Store.getState("selectedGif");
     // debugger;
     return (
       <div>
-        {seletedGif.map((item) => <img src={item.url}></img>)}
+        {seletedGif.map(item => (
+          <img src={item.url} />
+        ))}
+        <Link to="/result">
+        <Button variant="contained" color="primary">
+          Submit
+        </Button>
+        </Link>
       </div>
-    )
+    );
   }
 }
 export default Timeline;
