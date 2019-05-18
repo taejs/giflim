93cc68d6b030f26e4d8884a01aecd015b389a259
Tae <smireat1@gmail.com>
2019-05-18 14:20:08 +0900

    feat : load gifiy images

--- src/components/SearchGif.js
+++ src/components/SearchGif.js
@@ -2,12 +2,48 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class SearchGif extends Component {
+  constructor() {
+    super();
+    this.state = {
+      gifData: []
+    };
+  }
+
+  search(event) {
+    let keyword = "";
+    let url = keyword
+      ? "https://gdg-webtech-hackathon-backend.firebaseapp.com/api/gif/search"
+      : "https://gdg-webtech-hackathon-backend.firebaseapp.com/api/gif/random50";
+    axios
+      .get(url, {
+        q: keyword
+      })
+      .then(response => {
+        // handle success
+        this.setState({
+          gifData: response.data.data
+        });
+        console.log(response);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function() {
+        // always executed
+      });
+  }
+
   render() {
+    var gifData = this.state.gifData;
     return (
       <div>
-        asd
+        <button onClick={this.search.bind(this)} />
+        {gifData.map(function(item) {
+          return <img src={item.url} />;
+        })}
       </div>
-    )
+    );
   }
 }
 export default SearchGif;
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -4,7 +4,7 @@ class Timeline extends Component {
   render() {
     return (
       <div>
-
+        
       </div>
     )
   }
