b786ed2bee72c3d2cdf1267484e177d8207db34b
fed-gren <deveb22@gmail.com>
2019-05-18 15:16:18 +0900

    feat: create a text field, button for search

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import {BrowserRouter, Route} from 'react-router-dom'
 import WorkingPage from './components/WorkingPage';
 import ResultPage from './components/ResultPage';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <BrowserRouter>
         <Route path="/" exact component={WorkingPage}/>
         <Route path="/result/" component={ResultPage} />
--- src/components/SearchGif.js
+++ src/components/SearchGif.js
@@ -1,23 +1,27 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { bindCallback } from "rxjs";
+import { statement } from "@babel/template";
+import { Button, TextField } from "@material-ui/core";
+import "../styles/SearchGif.css";
 
 class SearchGif extends Component {
   constructor() {
     super();
     this.state = {
-      gifData: []
+      gifData: [],
+      searchText: ""
     };
   }
 
   search(event) {
-    let keyword = "";
+    let keyword = this.state.searchText;
+    console.log(`keyword : ${keyword}`);
     let url = keyword
-      ? "https://gdg-webtech-hackathon-backend.firebaseapp.com/api/gif/search"
+      ? `https://gdg-webtech-hackathon-backend.firebaseapp.com/api/gif/search?q=${keyword}`
       : "https://gdg-webtech-hackathon-backend.firebaseapp.com/api/gif/random50";
     axios
-      .get(url, {
-        q: keyword
-      })
+      .get(url, {})
       .then(response => {
         // handle success
         this.setState({
@@ -34,11 +38,34 @@ class SearchGif extends Component {
       });
   }
 
+  setSearchText(event) {
+    this.setState({
+      searchText: event.target.value
+    });
+    console.log(`searchText : ${this.state.searchText}`);
+  }
+
   render() {
     var gifData = this.state.gifData;
     return (
       <div>
-        <button onClick={this.search.bind(this)} />
+        <section className="serach">
+          <TextField
+            className="input_gif_search"
+            label="search yout GIF!"
+            margin="normal"
+            onChange={this.setSearchText.bind(this)}
+            // onKeyDown={this.setSearchText(this)}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.search.bind(this)}
+            className="search_button"
+          >
+            Search
+          </Button>
+        </section>
         {gifData.map(function(item) {
           return <img src={item.url} />;
         })}
